399699c5ee2255b09be1b5b66651e10b62c46ee2
Mahfuzur Rahman Shabbir <mahfuzurrahmanshabbir@gmail.com>
2024-03-16 06:48:28 +0600

    baseURL added

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosSecure = axios.create({
-    // baseURL: "https://mahfuz-s-portfolio-website-server.vercel.app",
-  baseURL: "http://localhost:5000",
+    baseURL: "https://baraqa-properties-server.vercel.app",
+  // baseURL: "http://localhost:5000",
 });
 const useAxiosPublic = () => {
   return axiosSecure;
