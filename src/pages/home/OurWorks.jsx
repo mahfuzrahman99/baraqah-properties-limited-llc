a48e3b8fdbf7377d7402bc741d22eb616c912afe
Mahfuzur Rahman Shabbir <mahfuzurrahmanshabbir@gmail.com>
2024-03-28 06:13:30 +0600

    Our works component updated

--- src/pages/home/OurWorks.jsx
+++ src/pages/home/OurWorks.jsx
@@ -10,15 +10,20 @@ AOS.init();
 const OurWorks = () => {
   const [demos] = useFetchDemos();
   const [searchedDemo, setSearchedDemo] = useState();
-  
+  const [searchText, setSearchTex] = useState("");
+
+  const handleInputText = (e) => {
+    console.log(e.key)
+  };
+
   const handleSearchBlog = (e) => {
     e.preventDefault();
     const enteredSearchValue = e.target.search.value.toLowerCase();
     e.target.reset();
 
     if (enteredSearchValue.length >= 1) {
-      const filteredDemo = demos?.filter(
-        (demo) => demo?.demo_category.toLowerCase().includes(enteredSearchValue)
+      const filteredDemo = demos?.filter((demo) =>
+        demo?.demo_category.toLowerCase().includes(enteredSearchValue)
       );
 
       setSearchedDemo(filteredDemo);
@@ -36,18 +41,20 @@ const OurWorks = () => {
       <div className="md:flex justify-between max-w-5xl mx-auto md:mt-16 md:mb-6">
         <div></div>
         <div>
-        <form onSubmit={handleSearchBlog} className="join">
-          <input
-            type="text"
-            name="search"
-            className="hover:border-none px-5 join-item outline-none w-[250px]"
-            placeholder="Search a demo by category..."
-          />
-          <button className="btn join-item rounded-r-md bg-[#a9a3cf] hover:bg-[#311A36] transform duration-1000 text-white p-2">
-            SEARCH
-          </button>
-        </form>
-      </div>
+          <form onSubmit={handleSearchBlog} className="join">
+            <input
+              onKeyUp={handleInputText}
+              value={searchText}
+              type="text"
+              name="search"
+              className="hover:border-none px-5 join-item outline-none w-[250px]"
+              placeholder="Search a demo by category..."
+            />
+            <button className="btn join-item rounded-r-md bg-[#a9a3cf] hover:bg-[#311A36] transform duration-1000 text-white p-2">
+              SEARCH
+            </button>
+          </form>
+        </div>
       </div>
       <div className="max-w-5xl mx-auto">
         {demosToDisplay?.map((demo) => (
