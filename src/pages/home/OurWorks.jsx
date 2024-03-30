43b6231c2fe18f15a143ecad8acb04773f99906b
Mahfuzur Rahman Shabbir <mahfuzurrahmanshabbir@gmail.com>
2024-03-30 17:17:27 +0600

    update

--- src/pages/home/OurWorks.jsx
+++ src/pages/home/OurWorks.jsx
@@ -18,9 +18,7 @@ const OurWorks = () => {
     <div>
       <Navbar />
       <div className="md:flex justify-between max-w-5xl mx-auto md:mt-16 md:mb-6">
-        <div>
-          {/* this div for flex between */}
-        </div>
+        <div>{/* this div for flex between */}</div>
         <SearchDemo demos={demos} setSearchedDemo={setSearchedDemo} />
       </div>
       <div className="max-w-5xl mx-auto">
