b2118369833c039f5318897bc7ed5480b83173cb
mahfuzrahman99 <mahfuzurrahmanshabbir@gmail.com>
2024-04-03 12:22:43 +0600

    updadte

--- src/pages/dashboard/AllUsers.jsx
+++ src/pages/dashboard/AllUsers.jsx
@@ -45,7 +45,7 @@ const AllUsers = () => {
   };
 
   return (
-    <div className="max-w-5xl mx-auto w-[95vw] md:w-auto h-[69vh]">
+    <div className="max-w-5xl mx-auto w-[95vw] md:w-auto h-[80vh]">
       <div className="mt-5 md:mt-20">
         <button
           onClick={() => setShowModal(true)}
